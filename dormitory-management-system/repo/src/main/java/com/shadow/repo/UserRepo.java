4994a4161ab6a4deae39953ff7af62fcb9446d96
changzhengwang <904185042@qq.com>
2020-03-13 09:43:59 +0800

    first commit

---
+++ dormitory-management-system/repo/src/main/java/com/shadow/repo/UserRepo.java
@@ -0,0 +1,8 @@
+package com.shadow.repo;
+
+import com.shadow.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface UserRepo extends JpaRepository<User, Integer> {
+}
---
+++ dormitory-management-system/service/src/main/java/com/shadow/service/UserService.java
@@ -0,0 +1,10 @@
+package com.shadow.service;
+
+import com.shadow.entity.User;
+
+import java.util.List;
+
+public interface UserService {
+
+    List<User> getUsers();
+}
