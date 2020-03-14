49e358b4179e78d8c5c1775b3522121763f01baf
changzhengwang <904185042@qq.com>
2020-03-14 11:59:25 +0800

    333

--- dormitory-management-system/repo/src/main/java/com/shadow/repo/UserRepo.java
+++
@@ -1,8 +0,0 @@
-package com.shadow.repo;
-
-import com.shadow.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface UserRepo extends JpaRepository<User, Integer> {
-}
